ae50ee1a7a7afc0b27ec80da14e7ac4edf8bcb9c
manuelsansores <manuelsansoresg@gmail.com>
2023-01-12 21:03:26 -0600

    change

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,8 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data_user = array('name' => 'manuel',
-        'email' => 'vichsoriano@gmail.com', 'password' => bcrypt('@Soriano2022'));
+        'email' => 'manuelsansoresg@gmail.com', 'password' => bcrypt('demor00txx'));
+        //'email' => 'vichsoriano@gmail.com', 'password' => bcrypt('@Soriano2022'));
         $user = new User($data_user);
         $user->save();
     }
